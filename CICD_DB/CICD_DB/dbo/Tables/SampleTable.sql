34fad7b95a661fd38098c77148f608e551a06994
Satheesh <r.satheesh@altrosyn.com>
2024-03-02 12:49:55 +0530

    Load Initial Solution

---
+++ CICD_DB/CICD_DB/dbo/Tables/SampleTable.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SampleTable] (
+    [ID]          INT            NOT NULL,
+    [Name]        NVARCHAR (50)  NULL,
+    [Age]         INT            NULL,
+    [Email]       NVARCHAR (100) NULL,
+    [CreatedDate] DATETIME       NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
